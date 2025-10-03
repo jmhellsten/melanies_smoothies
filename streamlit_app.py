f5e55902322ba3d24274ecafffd27aa46efc54ea
Juha-Matti Hellsten <jmhellst@gmail.com>
2025-10-03 16:03:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,18 +19,39 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop() 
 
-ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe)
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()  # <-- TÄHÄN KOHTAAN
+# st.dataframe(pd_df)
+# st.stop() 
+
+ingredients_list = st.multiselect(
+'Choose up to 5 ingredients:',
+my_dataframe, # TAI pd_df jos olet päivittänyt sen
+max_selections=5
+)
 
 if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+      ingredients_string += fruit_chosen + ' '
+    
+      search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    
+      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+      st.subheader(fruit_chosen + ' Nutrition Information')
+    
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+    
+      try:
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+      except:
+        st.error("Ravitsemustietoa ei löytynyt.") # Lisätty virhekäsittely
+
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -39,6 +60,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
 
 
 
